fbe53cef0d9f8d83e777c8f03c3f96a96d6120f3
Eruin <eruin.silva@gmail.com>
2020-08-05 17:52:44 -0300

    up

---
+++ src/hello/hello.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+
+	exibeIntroducao()
+
+	for {
+		exibeMenu()
+
+		comando := leComando()
+
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo logs")
+		case 0:
+			fmt.Println("Saindo do programa")
+			os.Exit(0)
+		default:
+			fmt.Println("Não reconheço esse comando")
+			os.Exit(-1)
+		}
+	}
+}
+
+func exibeIntroducao() {
+	nome := "Eruin"
+	versao := 1.2
+	fmt.Println("Olá, sr.", nome)
+	fmt.Println("Este programa esta na versão", versao)
+
+}
+
+func exibeMenu() {
+	fmt.Println("1- Iniciar monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do programa")
+
+}
+
+func leComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("O comando escolhido foi ", comandoLido)
+
+	return comandoLido
+}
+
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando...")
+	fmt.Println("Exibindo logs...")
+	site := "https://www.alura.com.br/"
+	resp, _ := http.Get(site)
+	//fmt.Println(resp)
+
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "foi carregado com sucesso!")
+	} else {
+		fmt.Println("Site:", site, "esta com problemas. Status Code", resp.StatusCode)
+	}
+}
